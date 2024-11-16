83c7a2eeac08f11ebad51d965e7a1860f3cae4b3
CoderXam <maxdaniellevine@gmail.com>
2024-11-16 10:14:17 -0500

    game loop and player movement

--- main.py
+++ main.py
@@ -1,1 +1,68 @@
-print("Hello Wolrd")
+import pygame, sys
+from pygame.locals import *
+import random
+ 
+pygame.init()
+ 
+FPS = 60
+FramePerSec = pygame.time.Clock()
+ 
+# Predefined some colors
+BLUE  = (0, 0, 255)
+RED   = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+ 
+# Screen information
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+ 
+DISPLAYSURF = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+DISPLAYSURF.fill(WHITE)
+pygame.display.set_caption("Game")
+ 
+ 
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__() 
+        self.image = pygame.image.load("Sprites/Player.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = (160, 520)
+ 
+    def update(self):
+        pressed_keys = pygame.key.get_pressed()
+        if pressed_keys[K_w]:
+            self.rect.move_ip(0, -5)
+        if pressed_keys[K_s]:
+            self.rect.move_ip(0,5)
+        if pressed_keys[K_a]:
+            self.rect.move_ip(-5, 0)
+        if pressed_keys[K_d]:
+            self.rect.move_ip(5,0)
+         
+        if self.rect.left > 0:
+              if pressed_keys[K_LEFT]:
+                  self.rect.move_ip(-5, 0)
+        if self.rect.right < SCREEN_WIDTH:        
+              if pressed_keys[K_RIGHT]:
+                  self.rect.move_ip(5, 0)
+ 
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)     
+ 
+         
+P1 = Player()
+ 
+while True:     
+    for event in pygame.event.get():              
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    P1.update()
+     
+    DISPLAYSURF.fill(WHITE)
+    P1.draw(DISPLAYSURF)
+         
+    pygame.display.update()
+    FramePerSec.tick(FPS)
