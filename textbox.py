be956549a83350be4d4525b5d37a2dcee2f95ec5
CoderXam <maxdaniellevine@gmail.com>
2024-11-16 17:17:07 -0500

    text stuff for levels

--- textbox.py
+++ textbox.py
@@ -6,10 +6,20 @@ pygame.font.init()
 my_font = pygame.font.SysFont('Comic Sans MS', 30)
 
 TEXT = []
-TEXT.append(my_font.render('You must survive 5 nights at freddy fivebears.', True, WHITE))
+TEXT.append(my_font.render('You must determine the colour of each room, but alas, you are colourblind!', True, WHITE))
+TEXT.append(my_font.render(' ', True, WHITE))
 TEXT.append(my_font.render('Press F to bring up lens.', True, WHITE))
 TEXT.append(my_font.render('Press G tp bring up diffraction gratting', True, WHITE))
 TEXT.append(my_font.render('Press H tp bring up refraction stuff', True, WHITE))
+
+TEXTLEVEL1=my_font.render('You see a light at the end of the room. Maybe you have something to focus on it.')
+
+TEXTLEVEL2=my_font.render('Theres a plane with a grating, the light shines through it.')
+
+TEXTLEVEL3=my_font.render('Theres pieces of glass.')
+
+TEXTEND1=my_font.render('You made it to the end! With the information you have gathered you must determine', True, WHITE)
+TEXTEND2=my_font.render('which type of colourblindness you have.', True, WHITE)
 
 LEVELS=[]
 for i in range(3):
