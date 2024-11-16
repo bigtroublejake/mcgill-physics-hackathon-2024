0d303f84894e9fcc65241cd8b9ca0dbba81c6039
CoderXam <maxdaniellevine@gmail.com>
2024-11-16 14:18:47 -0500

    room stuff epic

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 import pygame, sys
 from pygame.locals import *
-from PIL import Image, ImageFilter
-
-
 from parameters import *
 from fnaf_cam import Fnaf_cam, ballBoy
-
+from roombuilder import roomBuidler
+from room import Room
 
 pygame.init()
  
 FPS = 60
 FramePerSec = pygame.time.Clock()
- 
-
  
 DISPLAYSURF = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 pygame.display.set_caption("Game")
@@ -27,28 +23,33 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.transform.scale_by(self.image, 0.25)
         self.rect = self.image.get_rect()
         self.shown = 1
+        self.current_room=0
 
     def update(self):
         pressed_keys = pygame.key.get_pressed()
         
         if pressed_keys[K_w]:
             self.rect.move_ip(0, -5)
-        if pressed_keys[K_s]:
+        if pressed_keys[K_s]and self.rect.centery<SCREEN_HEIGHT:
             self.rect.move_ip(0,5)
-        if pressed_keys[K_a]:
+        if pressed_keys[K_a] and self.rect.centerx>0:
             self.rect.move_ip(-5, 0)
-        if pressed_keys[K_d]:
+        if pressed_keys[K_d] and self.rect.centerx<SCREEN_WIDTH:
             self.rect.move_ip(5,0)
-        # if pressed_keys[K_f]:
-        #     lens.toggle()
-         
+
+        if self.rect.centery==0:
+            self.rect.centery=SCREEN_HEIGHT
+            self.current_room+=1
+            print(self.current_room)
+
+        '''
         if self.rect.left > 0:
               if pressed_keys[K_LEFT]:
                   self.rect.move_ip(-5, 0)
         if self.rect.right < SCREEN_WIDTH:        
               if pressed_keys[K_RIGHT]:
                   self.rect.move_ip(5, 0)
-    
+        '''  
 
     def state_toggle(self):
         self.shown = not self.shown
@@ -59,7 +60,13 @@ class Player(pygame.sprite.Sprite):
         if self.shown == 1:
             surface.blit(self.image, self.rect)  
  
-         
+# Create rooms 
+builder = roomBuidler()
+roomSize = [800, 1200]
+rooms = []
+for i in range(3):
+    rooms.append(Room(builder.cbroomColorRGB[i],roomSize[0],roomSize[1],SCREEN_WIDTH/2,0))
+
 P1 = Player()
  
 while True:     
@@ -80,12 +87,13 @@ while True:
                     lensLight.blurAdd(-1)
 
 
-
-
-
     P1.update()
      
     DISPLAYSURF.fill(GRAY)
+    
+    if P1.current_room!=0:
+        rooms[P1.current_room-1].draw(DISPLAYSURF)
+    
     P1.draw(DISPLAYSURF)
     lens.draw(DISPLAYSURF)
     lensLight.draw(DISPLAYSURF)
--- room.py
+++ room.py
@@ -2,11 +2,12 @@ import pygame
 from pygame.locals import *
 
 class Room(pygame.sprite.Sprite): 
-    def __init__(self, color, height, width): 
+    def __init__(self, color, width, height, posx, posy): 
         super().__init__() 
         self.image = pygame.Surface([width, height]) 
         self.image.fill(color)
         self.rect = self.image.get_rect()
+        self.rect.center = (posx,posy)
 
     def draw(self, surface):
         surface.blit(self.image, self.rect)
