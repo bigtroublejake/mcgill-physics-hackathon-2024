bf040aee25170fe136f7b12a0114fec9c6d67220
CoderXam <maxdaniellevine@gmail.com>
2024-11-16 12:03:04 -0500

    lens stuff

--- fnaf_cam.py
+++ fnaf_cam.py
@@ -1,18 +1,19 @@
-import pygame, sys
+import pygame
 from pygame.locals import *
 
 class Fnaf_cam(pygame.sprite.Sprite):
-    def __init__(self, image):
+    def __init__(self):
         super().__init__() 
-        self.image = image
+        self.image = pygame.image.load("Sprites/lens.png")
         self.rect = self.image.get_rect()
         self.shown = 0
 
-    def show(self):
-        self.shown = 1
-
-    def hide(self):
-        self.shown = 0
+    def toggle(self):
+        print("toggle lens")
+        if (self.shown==1):
+            self.shown=0
+        elif (self.shown==0):
+            self.shown=1
  
     def draw(self, surface):
         if (self.shown == 1):
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame, sys
 from pygame.locals import *
+from fnaf_cam import Fnaf_cam
  
 pygame.init()
  
@@ -21,18 +22,18 @@ SCREEN_HEIGHT = 900
 DISPLAYSURF = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 pygame.display.set_caption("Game")
  
- 
+lens = Fnaf_cam()
+
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__() 
         self.image = pygame.image.load("Sprites/Player.png")
         self.image = pygame.transform.scale_by(self.image, 0.25)
         self.rect = self.image.get_rect()
-        self.rect.center = (160, 520)
-    
- 
+
     def update(self):
         pressed_keys = pygame.key.get_pressed()
+        
         if pressed_keys[K_w]:
             self.rect.move_ip(0, -5)
         if pressed_keys[K_s]:
@@ -41,6 +42,8 @@ class Player(pygame.sprite.Sprite):
             self.rect.move_ip(-5, 0)
         if pressed_keys[K_d]:
             self.rect.move_ip(5,0)
+        if pressed_keys[K_f]:
+            lens.toggle()
          
         if self.rect.left > 0:
               if pressed_keys[K_LEFT]:
@@ -50,7 +53,7 @@ class Player(pygame.sprite.Sprite):
                   self.rect.move_ip(5, 0)
  
     def draw(self, surface):
-        surface.blit(self.image, self.rect)     
+        surface.blit(self.image, self.rect)  
  
          
 P1 = Player()
@@ -64,6 +67,7 @@ while True:
      
     DISPLAYSURF.fill(GRAY)
     P1.draw(DISPLAYSURF)
+    lens.draw(DISPLAYSURF)
          
     pygame.display.update()
     FramePerSec.tick(FPS)
