581e27355203dd7919b8eb8b5844c6ded1c1d6f2
bigtroublejake <bigtroublejake@gmail.com>
2024-11-16 12:38:18 -0500

    created light + some architect changes

--- fnaf_cam.py
+++ fnaf_cam.py
@@ -1,20 +1,40 @@
 import pygame
 from pygame.locals import *
 
+from parameters import *
 class Fnaf_cam(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__() 
         self.image = pygame.image.load("Sprites/lens.png")
+
+        
         self.rect = self.image.get_rect()
+        self.rect.center = (SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
         self.shown = 0
 
     def toggle(self):
         print("toggle lens")
-        if (self.shown==1):
-            self.shown=0
-        elif (self.shown==0):
-            self.shown=1
+        self.shown = not self.shown
  
     def draw(self, surface):
         if (self.shown == 1):
             surface.blit(self.image, self.rect)
+
+class ballBoy(pygame.sprite.Sprite):
+    def __init__(self):
+        self.image = pygame.image.load("Sprites/BallLight.png")
+        self.image = pygame.transform.scale_by(self.image, 0.1)
+
+        self.rect = self.image.get_rect()
+
+        self.rect.center = (SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+        self.shown = 0
+    
+
+    def draw(self, surface):
+        if self.shown == 1:
+            surface.blit(self.image, self.rect)
+
+    def toggle(self):
+        print("toggle light")
+        self.shown = not self.shown
--- main.py
+++ main.py
@@ -1,28 +1,24 @@
 import pygame, sys
 from pygame.locals import *
-from fnaf_cam import Fnaf_cam
- 
+from PIL import Image, ImageFilter
+
+
+from parameters import *
+from fnaf_cam import Fnaf_cam, ballBoy
+
+
 pygame.init()
  
 FPS = 60
 FramePerSec = pygame.time.Clock()
  
-# Predefined some colors
-BLUE  = (0, 0, 255)
-RED   = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-GRAY = (128, 128, 128)
- 
-# Screen information
-SCREEN_WIDTH = 1200
-SCREEN_HEIGHT = 900
+
  
 DISPLAYSURF = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 pygame.display.set_caption("Game")
  
 lens = Fnaf_cam()
+lensLight = ballBoy()
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
@@ -30,6 +26,7 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.image.load("Sprites/Player.png")
         self.image = pygame.transform.scale_by(self.image, 0.25)
         self.rect = self.image.get_rect()
+        self.shown = 1
 
     def update(self):
         pressed_keys = pygame.key.get_pressed()
@@ -54,10 +51,13 @@ class Player(pygame.sprite.Sprite):
     
 
     def state_toggle(self):
+        self.shown = not self.shown
         lens.toggle()
+        lensLight.toggle()
  
     def draw(self, surface):
-        surface.blit(self.image, self.rect)  
+        if self.shown == 1:
+            surface.blit(self.image, self.rect)  
  
          
 P1 = Player()
@@ -67,6 +67,8 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+        
+        ### USE THIS FOR ONE TIME KEY PRESSES ###
         elif event.type == KEYDOWN: # Detect single key presses
                 
                 if event.key == K_f:
@@ -82,6 +84,8 @@ while True:
     DISPLAYSURF.fill(GRAY)
     P1.draw(DISPLAYSURF)
     lens.draw(DISPLAYSURF)
-         
+    lensLight.draw(DISPLAYSURF)
+
+
     pygame.display.update()
     FramePerSec.tick(FPS)
