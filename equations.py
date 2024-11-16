9931fadbf54683896d05c6fad75a50be27950469
bigtroublejake <bigtroublejake@gmail.com>
2024-11-16 15:17:07 -0500

    diffraction equation

--- equations.py
+++ equations.py
@@ -6,3 +6,24 @@ def angularResolution(diameter, waveLength):
     "returns the angular resolution when the diameter was changed"
     return ((1.22)*(diameter/waveLength))
 
+def diffractionGrating(wavelength) -> list:
+    "returns a list of the position of the nodes"
+    d = 5e-6
+    L = 100
+    ls = []
+    var = 0
+
+    for m in range(7):
+        
+        angle = np.arcsin(m * wavelength / d)
+        y = L * np.tan(angle)
+
+        ls.append(y)
+
+        # print(y-var)
+        # var = y
+
+    return(ls)
+
+print(diffractionGrating(670e-9))
+
