c5a2b1e8d470953000884b8b22e9a63d2386f77f
Tristan Giannopoulos <tristan.giannopoulos@gmail.com>
2024-11-17 10:56:08 -0500

    added floor texturing

--- room.py
+++ room.py
@@ -4,10 +4,14 @@ from pygame.locals import *
 class Room(pygame.sprite.Sprite): 
     def __init__(self, color, width, height, posx, posy): 
         super().__init__() 
-        self.image = pygame.Surface([width, height]) 
+        self.image = pygame.Surface([width, height])
+        self.floor = pygame.image.load("Sprites/room_floor.png")
         self.image.fill(color)
         self.rect = self.image.get_rect()
         self.rect.center = (posx,posy)
+        self.frect = self.floor.get_rect()
+        self.frect.center = (posx,posy)
 
     def draw(self, surface):
         surface.blit(self.image, self.rect)
+        surface.blit(self.floor, self.frect)
