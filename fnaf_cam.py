6168976d9f9312447dd8e78266c2ef4f60f53d50
CoderXam <maxdaniellevine@gmail.com>
2024-11-16 20:32:31 -0500

    blur starts at zero

--- fnaf_cam.py
+++ fnaf_cam.py
@@ -32,10 +32,10 @@ class ballBoy(pygame.sprite.Sprite):
         self.shown = False
 
         # Create blurred image
-        self.blur_amount = 17  # Adjust this value to control blur amount
+        self.blur_amount = 0  # Adjust this value to control blur amount
         
         size = self.original_image.get_size()
-        smaller_img = pygame.transform.smoothscale(self.original_image, (int(size[0]/self.blur_amount), int(size[1]/self.blur_amount)))
+        smaller_img = self.original_image
         self.blurred_image = pygame.transform.smoothscale(smaller_img, size)
         self.blurAdd(0)
 
@@ -61,7 +61,6 @@ class ballBoy(pygame.sprite.Sprite):
             self.blurred_image = pygame.transform.scale_by(self.blurred_image, self.scale_amount)
 
 
-
         if self.blur_amount == 0:
             self.blurred_image = self.original_image
         elif abs(self.blur_amount) > 30:
@@ -71,11 +70,9 @@ class ballBoy(pygame.sprite.Sprite):
             smaller_img = pygame.transform.smoothscale(self.original_image, (int(size[0]/abs(self.blur_amount)), int(size[1]/abs(self.blur_amount))))
             self.blurred_image = pygame.transform.smoothscale(smaller_img, size)
 
-
-
         self.rect = self.blurred_image.get_rect()
         self.rect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT //2)
-
+    
     def update(self):
         if self.shown == True:
             pressed_keys = pygame.key.get_pressed()
