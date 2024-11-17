cbecce3e75e49cf3943f1badcf5d71fd15cf366e
CoderXam <maxdaniellevine@gmail.com>
2024-11-17 08:34:18 -0500

    units

--- main.py
+++ main.py
@@ -203,8 +203,8 @@ while True:
     mysterydiff.draw(DISPLAYSURF)
     colorsdiff.draw(DISPLAYSURF)
     if lens.shown==1:
-        DISPLAYSURF.blit(SETTEXT('Angle of resolution = '+str(P1.angle), WHITE), (SCREEN_WIDTH/2-100, SCREEN_HEIGHT-80))
-        DISPLAYSURF.blit(SETTEXT('Measured wavelength = '+str(equations.wavelength(P1.angle, P1.diameter)), WHITE), (SCREEN_WIDTH/2-100, SCREEN_HEIGHT-60))
+        DISPLAYSURF.blit(SETTEXT('Angle of resolution = '+str(P1.angle)+' 10^-11 degrees', WHITE), (SCREEN_WIDTH/2-100, SCREEN_HEIGHT-80))
+        DISPLAYSURF.blit(SETTEXT('Measured wavelength = '+str(equations.wavelength(P1.angle, P1.diameter))+' nm', WHITE), (SCREEN_WIDTH/2-100, SCREEN_HEIGHT-60))
         DISPLAYSURF.blit(LENSINSTRUCTIONS, LENSINSTRUCTIONSRECT)
 
     snellLens.update()
